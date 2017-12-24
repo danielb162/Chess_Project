5a510e699fe6dc8564c89949e85629110f16b31c
H0rati0 <32465422+danielb162@users.noreply.github.com>
2017-12-23 21:51:38 -0500

    Test

---
+++ chess.h
@@ -0,0 +1,9 @@
+#ifndef CHESS_HEAD
+    #define CHESS_HEAD
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h> // For memset
+    #include <unistd.h>
+    #include <sys/wait.h>
+#endif
+
