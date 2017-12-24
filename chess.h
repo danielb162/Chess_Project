80de660ad2395c9e62957b69edeca7046ea9f8a4
Simon Zheng <simz97@outlook.com>
2017-12-24 03:58:31 -0500

    Implemented enums and Piece struct

--- chess.h
+++ chess.h
@@ -1,13 +1,33 @@
 #ifndef CHESS_HEAD
-    #define CHESS_HEAD
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h> // For memset
-    #include <unistd.h>
-    #include <sys/wait.h>
-#endif
+#define CHESS_HEAD
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // For memset
+#include <unistd.h>
+#include <sys/wait.h>
+
+typedef enum {
+    NONE,
+    KING,
+    QUEEN,
+    ROOK,
+    BISHOP,
+    KNIGHT,
+    PAWN
+} ID;
+
+typedef enum { BLACK, WHITE } Color;
+
+typedef struct PIECE {
+    ID id;
+    Color color;
+} Piece;
 
 typedef struct CELL {
     char id;
     char color;
+    Piece piece;
 } Cell;
+
+#endif /* CHESS_HEAD */
