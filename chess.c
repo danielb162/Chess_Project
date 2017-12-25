89b429af882ccf88632f2c7254d49c6e96396289
H0rati0 <32465422+danielb162@users.noreply.github.com>
2017-12-24 23:41:46 -0500

    Fixed directory structure

--- chess.c
+++ chess.c
@@ -1,91 +1,97 @@
 #include "chess.h"
 
-static const char* BLK = "\033[0;30m";
-static const char* RED = "\033[0;31m";
-static const char* GRN = "\033[0;32m";
-static const char* ORG = "\033[0;33m";
-static const char* BLU = "\033[0;34m";
-static const char* PRP = "\033[0;35m";
-static const char* CYN = "\033[0;36m";
+// Escape codes for colors, in-terminal
 static const char* LGR = "\033[0;37m";
-
 static const char* DGR = "\033[1;30m";
-static const char* LRD = "\033[1;31m";
-static const char* LGN = "\033[1;32m";
 static const char* YLW = "\033[1;33m";
-static const char* LBL = "\033[1;34m";
-static const char* LPR = "\033[1;35m";
-static const char* LCY = "\033[1;36m";
 static const char* WHT = "\033[1;37m";
+static const char* ORA = "\033[0;33m";
+static const char* LGN = "\033[1;32m";
 
+
+
+// [Row][Column], i.e. [0][0] -> A1 & [4][3] -> D5
 Cell board[8][8];
 
+// Initiliaze an empty board:
 void initializeBoard() {
-    Piece black_pawn = { .id = PAWN, .color = BLACK };
-    Piece white_pawn = { .id = PAWN, .color = WHITE };
-    Cell white_cell = { .id = 'P', .color = 'B', .piece = black_pawn };
-    Cell black_cell = { .id = 'P', .color = 'W', .piece = white_pawn };
-
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            board[i][j] = ((i+j)%2 == 0) ? white_cell : black_cell;
+    for (int r = 0; r <= 7; r++) {
+        for (int c = 0; c <= 7; c++) {
+            if(r == 0) {
+                board[r][c].color = 'W';
+            }
+            else if(r == 1) {
+                board[r][c].id = 'P';
+                board[r][c].color = 'W';
+            }
+            else if(r == 6) {
+                board[r][c].id = 'P';
+                board[r][c].color = 'B';
+            }
+            else if(r == 7) {
+                board[r][c].color = 'B';
+            }
+            else {
+                board[r][c].id = ' ';
+                board[r][c].color = ' ';
+            }
         }
     }
+
+    // Initializing the heavy pieces (white & black):
+    board[0][0].id = 'R';
+    board[0][1].id = 'N';
+    board[0][2].id = 'B';
+    board[0][3].id = 'Q';
+    board[0][4].id = 'K';
+    board[0][5].id = 'B';
+    board[0][6].id = 'N';
+    board[0][7].id = 'R';
+
+    board[7][0].id = 'R';
+    board[7][1].id = 'N';
+    board[7][2].id = 'B';
+    board[7][3].id = 'Q';
+    board[7][4].id = 'K';
+    board[7][5].id = 'B';
+    board[7][6].id = 'N';
+    board[7][7].id = 'R';
+
 }
 
-// TO DO: Change this so it prints out the colors of the board appropriately with DGREY for black pieces and WHITE for white pieces
-int printBoard() {
+//  Recall [0][0] -> A1 & [4][3] -> D5
+void printBoard() {
     // Top padding:
-    printf("%s-------------------------\n", YLW);
-    for (char row = 'A'; row <= 'H'; row++) { // Row counter, i.e. A - H
-        for (char col = '1'; col <= '8'; col++) { // Col counter, i.e. 1 - 8
-            char temp[2] = {row, col};
-            if (board[row-'A'][col-'1'].piece.color == BLACK)
-                printf("%s|%s%c%c", YLW, DGR, row, col);
-            else if (board[row-'A'][col-'1'].piece.color == WHITE)
-                printf("%s|%s%c%c", YLW, WHT, row, col);
-            else printf("%s|%s%c%c", RED, BLU, row, col);
+    printf("%s_________________\n", WHT);
+    for (int row = 7; row >= 0; row--) { // Row counter, i.e. 0 - 7 -> A - H
+        for (int col = 0; col <= 7; col++) {
+            if (board[row][col].color == 'B')
+                printf("%s|%s%c", WHT, YLW, board[row][col].id);
+            else if (board[row][col].color == 'W')
+                printf("%s|%s%c", WHT, LGN, board[row][col].id);
+            else printf("%s| ", WHT);
         }
-        printf("%s|\n", YLW);
+        printf("%s|\n", WHT);
     }
     // Bottom padding:
-    printf("%s-------------------------\n", YLW);
-
-    return 0;
+    printf("%s¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯%s\n", YLW, LGR);
 }
 
 
-/* Bash color codes (https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux)
-
-Black        0;30     Dark Gray     1;30
-Red          0;31     Light Red     1;31
-Green        0;32     Light Green   1;32
-Brown/Orange 0;33     Yellow        1;33
-Blue         0;34     Light Blue    1;34
-Purple       0;35     Light Purple  1;35
-Cyan         0;36     Light Cyan    1;36
-Light Gray   0;37     White         1;37
-And then use them like this in your script ('\033' is the escape character for '<Esc>'):
-
-    RED='\033[0;31m'
-    NC='\033[0m' # No Color
-    printf "I ${RED}love${NC} Stack Overflow\n"
-    echo -e "I ${RED}love${NC} Stack Overflow"
-
-*/
 
 int main() {
-    // Uses genCells.c to create a clean board state
-    // #include "clean_board"
  
     /* Sanity check for clean_board:
     printf("A8's id is: \033[1;37m%c\033[0m\nD7's id is: \033[1;30m%c\033[0m\n", A8.id, D7.id);
-    printf("A1's id is %c, it's color is %c\n", row1[0].id, row1[0].color);*/
+    printf("A1's id is %c, it's color is %c\n", row1[0].id, row1[0].color);
+    printf("Size is: %d\n", sizeof(board[6][0].id)); 
+    printf("A7's id is %c, it's color is %c\n", board[6][0].id, board[6][0].color);*/
 
+    // Setting up an empty board:
     initializeBoard();
+
+    // Printing the current board state:
     printBoard();
-
-
 
     // No errors encountered
     return 0;
--- chess.h
+++ chess.h
@@ -1,33 +1,14 @@
 #ifndef CHESS_HEAD
-#define CHESS_HEAD
+    #define CHESS_HEAD
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h> // For memset
+    #include <unistd.h>
+    #include <sys/wait.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h> // For memset
-#include <unistd.h>
-#include <sys/wait.h>
+    typedef struct CELL {
+        char id;
+        char color;
+    } Cell;
 
-typedef enum {
-    NONE,
-    KING,
-    QUEEN,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    PAWN
-} ID;
-
-typedef enum { BLACK, WHITE } Color;
-
-typedef struct PIECE {
-    ID id;
-    Color color;
-} Piece;
-
-typedef struct CELL {
-    char id;
-    char color;
-    Piece piece;
-} Cell;
-
-#endif /* CHESS_HEAD */
+#endif
