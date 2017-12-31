23c7927e5626771ff97d20bd596cd55a5a4c6780
Simon Zheng <simz97@outlook.com>
2017-12-31 17:40:32 -0500

    Moved stuff to header

--- chess.c
+++ chess.c
@@ -1,23 +1,5 @@
 #include "chess.h"
 
-// Escape codes for colors, in-terminal
-static const char* LGR = "\033[0;37m";
-static const char* DGR = "\033[1;30m";
-static const char* YLW = "\033[1;33m"; // Acts as black for color reasons
-static const char* LGN = "\033[1;32m"; // Acts as white for color reasons
-static const char* WHT = "\033[1;37m";
-static const char* ORA = "\033[0;33m";
-static const char* RED = "\033[0;31m";
-static const char* BLU = "\033[0;34m";
-static const char* BBG = "\033[40m";
-static const char* WBG = "\033[107m";
-static const char* DBG = "\033[49m";
-//static const char* EDGE = "\033[1;30m|"; // Edge between cells
-
-// Maybe use char constants for black and white piece char instead
-static const char B = 'B';
-static const char W = 'W';
-
 // [Row][Column], i.e. [0][0] -> A1 & [5][3] -> D6
 Cell board[8][8];
 
--- chess.h
+++ chess.h
@@ -1,5 +1,6 @@
 #ifndef CHESS_HEAD
     #define CHESS_HEAD
+
     #include <stdio.h>
     #include <stdlib.h>
     #include <stdbool.h>
@@ -11,8 +12,26 @@
         char color;
     } Cell;
 
+    // Escape codes for colors, in-terminal
+    static const char* LGR = "\033[0;37m";
+    static const char* DGR = "\033[1;30m";
+    static const char* YLW = "\033[1;33m"; // Acts as black for color reasons
+    static const char* LGN = "\033[1;32m"; // Acts as white for color reasons
+    static const char* WHT = "\033[1;37m";
+    static const char* ORA = "\033[0;33m";
+    static const char* RED = "\033[0;31m";
+    static const char* BLU = "\033[0;34m";
+    static const char* BBG = "\033[40m";
+    static const char* WBG = "\033[107m";
+    static const char* DBG = "\033[49m";
+    //static const char* EDGE = "\033[1;30m|"; // Edge between cells
+
+    // Maybe use char constants for black and white piece char instead
+    static const char B = 'B';
+    static const char W = 'W';
+
     void initializeBoard();
     void printBoard();
-    //void parseInput(char* pre, int* x1, int* y1, int* x2, int* y2);
+    void parseInput(char* pre, int* num1, int* num2);
     
 #endif
